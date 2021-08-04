0ed94f54684370eab9efa82eb7721c50eb265513
ravi0li <mikel.veg777@gmail.com>
2021-08-04 03:23:03 +0200

    v1.1

--- EverhoodCorridor.py
+++ EverhoodCorridor.py
@@ -1,14 +1,14 @@
-import time, sys
+import time, sys, win32gui
 from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
-count = 0
-meta_count = 0
+dodge_count = 0
+match_count = 0
 
 input("\n Will start in 2 seconds. Press any key...")
 time.sleep(2)
 
-def w_s():
+def passive_ws():
     keyboard.press("w")
     time.sleep(0.1)
     keyboard.release("w")
@@ -18,27 +18,64 @@ def w_s():
     keyboard.release("s")
     time.sleep(0.1)
 
+def dodge_ws():
+    keyboard.release("d")
+    time.sleep(0.1)
+    keyboard.press("w")
+    time.sleep(0.1)
+    keyboard.release("w")
+    time.sleep(0.1)
+    keyboard.press("d")
+    time.sleep(0.1)
+    keyboard.release("d")
+    time.sleep(0.1)
+    keyboard.press("s")
+    time.sleep(0.1)
+    keyboard.release("s")
+    time.sleep(0.1)
+
+def get_pixel_colour(i_x, i_y):
+    i_desktop_window_id = win32gui.GetDesktopWindow()
+    i_desktop_window_dc = win32gui.GetWindowDC(i_desktop_window_id)
+    long_colour = win32gui.GetPixel(i_desktop_window_dc, i_x, i_y)
+    i_colour = int(long_colour)
+    win32gui.ReleaseDC(i_desktop_window_id,i_desktop_window_dc)
+    return (i_colour & 0xff), ((i_colour >> 8) & 0xff), ((i_colour >> 16) & 0xff)
+
 try:
+    pixel_results_390_old = ""
+    pixel_results_395_old = ""
     keyboard.press("d")
     while True:
-        w_s()
+        passive_ws()
 
-        if count > 30:
-            meta_count += 1
-            sys.stdout.flush()
-            sys.stdout.write(f"\r Reached the 30 mark {meta_count} times.")
-            count = 0
-            keyboard.release("d")
-            time.sleep(0.1)
-            w_s()
+        pixel_results_390 = get_pixel_colour(390, 390)
+        pixel_results_395 = get_pixel_colour(395, 395)
+
+        sys.stdout.write(f" Dodge count: {dodge_count}  |  Pixel results: {pixel_results_390} - {pixel_results_395}         ")
+        sys.stdout.write("\b" * 80)
+        sys.stdout.flush()
+
+        if (pixel_results_390 == pixel_results_390_old) and (pixel_results_395 == pixel_results_395_old):
+            if (pixel_results_390 != (0, 0, 0)) and (pixel_results_390 != (0, 0, 0)):
+                match_count += 1
+
+        if match_count > 3:
+            dodge_count += 1
+            match_count = 0
+            dodge_ws()
             keyboard.press("d")
 
-        count += 1
+        pixel_results_390_old = pixel_results_390
+        pixel_results_395_old = pixel_results_395
+
 except KeyboardInterrupt:
     keyboard.release("d")
     exit(1)
-except Exception:
+except Exception as e:
     keyboard.release("d")
+    print(f"\n\n {e}")
+    print()
     exit(1)
 
 exit(1)
